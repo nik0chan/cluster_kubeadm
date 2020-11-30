1cc0cd1d4d5b3e47db888b09e2f363ce06029c2a
Nicolás Mantilla Simón <nicolasmantillasimon@gmail.com>
2020-11-30 22:41:01 +0100

    start worker

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -7,7 +7,7 @@
 #ansible-playbook -i hosts docker-storage.yml
 
 # Deploy master/s
-ansible-playbook -i hosts docker-masters.yml
+#ansible-playbook -i hosts docker-masters.yml
 
 # Deploy worker/s
-#ansible-playbook -i hosts docker-workers.yml
+ansible-playbook -i hosts docker-workers.yml
